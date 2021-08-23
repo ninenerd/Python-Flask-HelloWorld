1921baba00864b2bc6929fffd1c85a30992f1962
ninenerd <iamgvish@gmail.com>
2021-08-23 19:12:59 +0530

    Python Flask

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
+from flask import Flask, request, jsonify
+app=Flask(__name__)
+import random
 
-if __name__ == '__main__':
-  app.run()
+@app.route('/',methods=['GET','POST'])
+def test():
+    if request.method=='GET':
+        return jsonify({'reponse':"received,OK "})
+
+@app.route('/square')       
+def square():
+    if request.method=='GET':
+        return jsonify({"square":random.randint(11,111)**2})
+
+
+@app.route('/sqrt')       
+def sqrt():
+    if request.method=='GET':
+        return jsonify({"sqrt":random.randint(11,111)**(0.5)})
+
+
+if __name__ =='__main__':
+    app.run()
+
